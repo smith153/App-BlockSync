ba5cd7437e5a8c95b83e24f6a5424991ee300aa4
Samuel Smith <esaym@cpan.org>
2016-01-28 22:43:35 -0600

    Add mod_time, move compressed up one layer.

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,9 @@ CREATE TABLE file (
     path        TEXT NOT NULL,
     filename    TEXT NOT NULL,
     crcsum      VARCHAR(64) NOT NULL,
-    block_size  INTEGER NOT NULL
+    mod_time    INTEGER NOT NULL,
+    block_size  INTEGER NOT NULL,
+    compressed  BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 ALTER TABLE file ADD CONSTRAINT file_pkey PRIMARY KEY (ufn);
@@ -19,8 +21,7 @@ DROP TABLE IF EXISTS file_block CASCADE;
 CREATE TABLE file_block (
     file        VARCHAR(64) NOT NULL,
     id          INTEGER NOT NULL,
-    crcsum      VARCHAR(64) NOT NULL,
-    compressed  BOOLEAN NOT NULL DEFAULT FALSE
+    crcsum      VARCHAR(64) NOT NULL
 );
 
 ALTER TABLE file_block ADD CONSTRAINT file_block_pkey PRIMARY KEY (file,id);
