68650297964711dc35a7af720f82d6acdff6bd12
Samuel Smith <esaym@cpan.org>
2016-02-07 23:52:42 -0600

    Add post '/file', post '/block', file_sum_compare(), file_shorten()

--- lib/App/BlockSync/Server.pm
+++ lib/App/BlockSync/Server.pm
@@ -4,7 +4,9 @@ use Dancer2::Plugin::DBIC qw(rset);
 use IO::File;
 use Compress::Zlib qw(uncompress);
 use MIME::Base64 qw ( encode_base64 decode_base64);
+use Digest::MD5 qw(md5_hex);
 use File::Path qw(make_path);
+use File::Temp qw(tempfile);
 
 our $VERSION = '0.1';
 use Data::Dumper;
@@ -65,7 +67,7 @@ get '/block/:ufn/:id' => sub {
     my $block_id = params->{id};
 
     my $rs = rset('FileBlock')->search(
-        { 'me.file' => $ufn, id => $block_id },
+        { 'file' => $ufn, id => $block_id },
         {
             result_class => 'DBIx::Class::ResultClass::HashRefInflator',
         }
@@ -98,19 +100,19 @@ get '/delete/:ufn' => sub {
 };
 
 post '/new' => sub {
-    my $json = request->data;
+    my $file = request->data;
     my $rs;
     my $path;
-    my $compressed = ( exists $json->{compressed} && $json->{compressed} );
-    my $block_size = $json->{block_size};
+    my $compressed = ( exists $file->{compressed} && $file->{compressed} );
+    my $block_size = $file->{block_size};
 
     eval {
         $path = create_file_path(
-            $json->{hostname}, $json->{uhn},
-            $json->{path},     $json->{filename}
+            $file->{hostname}, $file->{uhn},
+            $file->{path},     $file->{filename}
         );
 
-        foreach my $block ( @{ $json->{file_blocks} } ) {
+        foreach my $block ( @{ $file->{file_blocks} } ) {
 
             my $seek = $block->{id} * $block_size;
             write_block( $seek, $block, $compressed, $path );
@@ -118,8 +120,10 @@ post '/new' => sub {
             delete $block->{data};    #don't store to DB
         }
 
-        #warn Dumper $json;
-        rset('File')->create($json);
+        file_sum_compare( $path, $file->{crcsum} ) or die "Sum missmatch!";
+
+        #warn Dumper $file;
+        rset('File')->create($file);
     };
 
     if ($@) {
@@ -131,11 +135,92 @@ post '/new' => sub {
 
 };
 
-post '/block' => sub {
-    my $json = request->data;
-    warn Dumper $json;
+post '/file' => sub {
+    my $file = request->data;
+    warn Dumper $file;
+
+    my $rs;
+    eval { $rs = rset('File')->update($file); };
+
+    if ($@) {
+        warn $@;
+        return { success => 0, error => ( split( /\n/, $@ ) )[0] };
+    } else {
+        return { success => 1, error => 0 };
+    }
 
 };
+
+post '/block' => sub {
+    my $file = request->data;
+    warn Dumper $file;
+
+    my $rs;
+    my $path;
+    my $compressed = ( exists $file->{compressed} && $file->{compressed} );
+    my $block_size;
+    my $block    = $file->{file_blocks}[0];
+    my $file_old = rset('File')->search(
+        {
+            ufn => $file->{ufn},
+            id  => $block->{id},
+
+        },
+        {
+            prefetch => 'file_blocks',
+        }
+    )->next();
+
+    eval {
+        $block_size = $file_old->block_size();
+        $path       = create_file_path(
+            $file->{hostname}, $file->{uhn},
+            $file->{path},     $file->{filename}
+        );
+
+        #shorten file if newer file is shorter
+        if ( $file->{file_size} < $file_old->file_size() ) {
+            file_shorten( $path, $file->{file_size}, );
+        }
+
+        my $seek = $block->{id} * $block_size;
+        write_block( $seek, $block, $compressed, $path );
+
+        delete $block->{data};    #don't store to DB
+
+        $file_old->dirty('1');
+        ( $file_old->file_blocks )[0]->update($block);
+        $file_old->update();
+    };
+
+    if ($@) {
+        warn $@;
+        return { success => 0, error => ( split( /\n/, $@ ) )[0] };
+    } else {
+        return { success => 1, error => 0 };
+    }
+
+};
+
+sub file_sum_compare
+{
+    my ( $file_path, $sum ) = @_;
+    my $md5 = Digest::MD5->new();
+    open( my $fh, "<$file_path" );
+    binmode($fh);
+
+    $md5->addfile($fh);
+    close($fh);
+
+    return $md5->hexdigest() eq $sum;
+}
+
+sub file_shorten
+{
+    my ( $file_path, $size ) = @_;
+    truncate( $file_path, $size ) or die "Couldn't shorten file $file_path $!";
+
+}
 
 sub create_file_path
 {
